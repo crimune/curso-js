42f0e1fa66e24bbe767e6487945bfcaba3496abc
crimun <crimun@gmail.com>
2024-07-11 17:51:38 -0400

    Evitando la propagación

--- burbujeo_captura.js
+++ burbujeo_captura.js
@@ -5,10 +5,24 @@ const hijo = document.querySelector('.border-secundary')
 const nieto = document.querySelector('.border-danger')
 
 // método addEventListener para escuchar el evento click, que desencadena una función flecha
-padre = document.addEventListener(("click") => console.log('Me diste click'))
-hijo = document.addEventListener(("click") => { 
-    console.log("Me diste click")
-})
-nieto = document.addEventListener(("click") => { 
-    console.log("Me diste click")
+
+// padre.addEventListener("click", () => {
+//     console.log("Click")
+// }, true)
+// hijo.addEventListener("click", () => {
+//     console.log("Click")
+// }, true)
+// nieto.addEventListener('click', () => {
+//     console.log("Click")
+// }, true)
+
+// Otra forma más eficienter de hacer lo mismo, pero evitando la propagación
+
+const divs = document.querySelectorAll(".border")  // Para asignar a una variable en JS a todos los divs con clase border
+
+divs.forEach((div) => {
+    div.addEventListener("click", (e) => {
+        e.stopPropagation()
+        console.log("Click")
+    })
 })
